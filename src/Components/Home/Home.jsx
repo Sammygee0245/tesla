de05c3c7058a611bb01b147040f07feeb6c22226
Sammy Gee <samuelbediako020@gmail.com>
2023-03-11 14:44:15 +0000

    secondcommit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,108 @@
 import React from "react";
 import "./Home.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSun, faDollarSign } from "@fortawesome/free-solid-svg-icons";
 
 function Home() {
   return (
     <div className="home-div">
+      <div
+        style={{
+          backgroundImage: `url("pics/Homepage-Model-3-Desktop-LHD.jpg")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="hpage1 one"
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <p className="sfer">Model 3</p>
+          <p>Leasing starting at $349/mo</p>
+        </div>
+        <div className="icns">
+          <button className="orderbtn ob">Custom Order</button>
+          <button className="orderbtn lb">Demo Drive</button>
+        </div>
+      </div>
+
+      <div
+        style={{
+          backgroundImage: `url("pics/Homepage-Model-Y-Global-Desktop.jpg")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="hpage1 one"
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <p className="sfer">Model Y</p>
+        </div>
+        <div className="icns">
+          <button className="orderbtn ob">Custom Order</button>
+          <button className="orderbtn lb">Demo Drive</button>
+        </div>
+      </div>
+
+      <div
+        style={{
+          backgroundImage: `url("pics/Homepage-Model-S-Desktop-LHD.jpg")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="hpage1 one"
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <p className="sfer">Model S</p>
+          <p className="scdd">Schedule a Demo Drive</p>
+        </div>
+        <div className="icns">
+          <button className="orderbtn ob">Custom Order</button>
+          <button className="orderbtn lb">View Inventory</button>
+        </div>
+      </div>
+
+      <div
+        style={{
+          backgroundImage: `url("pics/Homepage-Model-X-Desktop-LHD.jpg")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="hpage1 one"
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <p className="sfer">Model X</p>
+          <p className="scdd">Schedule a Demo Drive</p>
+        </div>
+        <div className="icns">
+          <button className="orderbtn ob">Custom Order</button>
+          <button className="orderbtn lb">View Inventory</button>
+        </div>
+      </div>
+
       <div
         style={{
           backgroundImage: `url("pics/firstpage.jpg")`,
@@ -14,37 +111,70 @@ function Home() {
         }}
         className="hpage1 one"
       >
-        <p className="sfer">Solar for Existing Roofs</p>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <p className="sfer">Solar Pannels</p>
+          <p>Lowest Cost Solar pannels in America and Ghana</p>
+        </div>
         <div className="icns">
-          <div className="ste">
-            <FontAwesomeIcon icon={faSun}></FontAwesomeIcon>
-            <h4>Convert Sunlight</h4>
-            <h5>to Energy</h5>
-          </div>
-
-          <div className="ste">
-            <FontAwesomeIcon icon={faDollarSign}></FontAwesomeIcon>
-            <h4>Guaranteed Lowest</h4>
-            <h5>for Solar</h5>
-          </div>
-
-          <div className="ste">
-            <h3>24/7</h3>
-            <h4>Energy</h4>
-            <h5>Monitoring</h5>
-          </div>
-
-          <div className="ste">
-            <button className="orderbtn">Order Now</button>
-          </div>
+          <button className="orderbtn ob">Order Now</button>
+          <button className="orderbtn lb">Learn More</button>
         </div>
       </div>
-      <div className="hpage1">p2</div>
-      <div className="hpage1">p3</div>
-      <div className="hpage1">p4</div>
-      <div className="hpage1">p5</div>
-      <div className="hpage1">p6</div>
-      <div className="hpage1">p7</div>
+
+      <div
+        style={{
+          backgroundImage: `url("pics/Homepage-SolarRoof-Desktop-Global.webp")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="hpage1 one"
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <p className="sfer">Solar Roof</p>
+          <p>Produce Clean Energy From Your Roof</p>
+        </div>
+        <div className="icns">
+          <button className="orderbtn ob">Order Now</button>
+          <button className="orderbtn lb">Learn More</button>
+        </div>
+      </div>
+
+      <div
+        style={{
+          backgroundImage: `url("pics/dd739764-bcaa-4263-9488-8c73bc9fb046.jpg")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="hpage1 one"
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <p className="sfer">Accessories</p>
+        </div>
+        <div className="icns">
+          <button className="orderbtn ob">Shop Now</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/NavBar/Nav.jsx
+++ src/Components/NavBar/Nav.jsx
@@ -9,89 +9,68 @@ function Nav() {
     setShownav(!shownav);
   }
 
-  console.log(shownav);
   return (
     <div>
       <nav className="navbar">
-        <img
-          src="pics\tesla-logo-png-20.png"
-          className="logo"
-          alt="Tesla logo"
-        />
+        <Link to="/">
+          <img
+            src="pics\tesla-logo-png-20.png"
+            className="logo"
+            alt="Tesla logo"
+          />
+        </Link>
 
         <ul className="ul1">
           <li className="list1">
-            <Link
-              to="mods"
-              className="link1"
-            >
+            <Link to="mods" className="link1">
               Model S
             </Link>
           </li>
           <li className="list1">
-            <Link
-              className="link1"
-              to="mod3"
-            >
+            <Link className="link1" to="mod3">
               Model 3
             </Link>
           </li>
           <li className="list1">
-            <Link
-              className="link1"
-              to="modx"
-            >
+            <Link className="link1" to="modx">
               Model X
             </Link>
           </li>
           <li className="list1">
-            <Link
-              className="link1"
-              to="mody"
-            >
+            <Link className="link1" to="mody">
               Model Y
             </Link>
           </li>
           <li className="list1">
-            <Link
-              className="link1"
-              to="solr"
-            >
+            <Link className="link1" to="solr">
               Solar Roof
             </Link>
           </li>
           <li className="list1">
-            <Link
-              className="link1"
-              to="solp"
-            >
+            <Link className="link1" to="solp">
               Solar Pannels
+            </Link>
+          </li>
+          <li className="list1">
+            <Link className="link1" to="solp">
+              Powerwall
             </Link>
           </li>
         </ul>
 
         <ul className="acc1">
           <li className="acclist1">
-            <Link
-              className="acclink1"
-              to="mods"
-            >
+            <Link className="acclink1" to="mods">
               Shop
             </Link>
           </li>
           <li className="acclist1">
-            <Link
-              className="acclink1"
-              to="mod3"
-            >
+            <Link className="acclink1" to="mod3">
               Account
             </Link>
           </li>
           <li className="acc  list1">
-            <Link
-              onClick={toggle}
-              className="acclink1"
-            >
+            <Link onClick={toggle} className="acclink1">
               Menu
             </Link>
           </li>
@@ -99,80 +78,55 @@ function Nav() {
       </nav>
       {shownav && (
         <div className="resNav">
-          <ul
-            onClick={toggle}
-            className="ul2"
-          >
+          <ul onClick={toggle} className="ul2">
             <li className="list1">
-              <Link
-                to="mods"
-                className="link1"
-              >
+              <Link to="mods" className="link1">
                 Model S
               </Link>
             </li>
             <li className="list1">
-              <Link
-                className="link1"
-                to="mod3"
-              >
+              <Link className="link1" to="mod3">
                 Model 3
               </Link>
             </li>
             <li className="list1">
-              <Link
-                className="link1"
-                to="modx"
-              >
+              <Link className="link1" to="modx">
                 Model X
               </Link>
             </li>
             <li className="list1">
-              <Link
-                className="link1"
-                to="mody"
-              >
+              <Link className="link1" to="mody">
                 Model Y
               </Link>
             </li>
             <li className="list1">
-              <Link
-                className="link1"
-                to="solr"
-              >
+              <Link className="link1" to="solr">
                 Solar Roof
               </Link>
             </li>
             <li className="list1">
-              <Link
-                className="link1"
-                to="solp"
-              >
+              <Link className="link1" to="solp">
                 Solar Pannels
               </Link>
             </li>
+            <li className="list1">
+              <Link className="link1" to="solp">
+                Powerwall
+              </Link>
+            </li>
             <li className="acclist1">
-              <Link
-                className="acclink1"
-                to="mods"
-              >
+              <Link className="acclink1" to="mods">
                 Shop
               </Link>
             </li>
             <li className="acclist1">
-              <Link
-                className="acclink1"
-                to="mod3"
-              >
+              <Link className="acclink1" to="mod3">
                 Account
               </Link>
             </li>
 
             <li className="acclist1">
-              <Link
-                className="acclink1"
-                to="/"
-              >
+              <Link className="acclink1" to="/">
                 Home
               </Link>
             </li>
